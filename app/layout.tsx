a8255b49bbc7ba2c7974ca38fde58538eaa435df
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-11-30 16:15:29 +0700

    feat: v0.2.0

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+})
+
+export const metadata: Metadata = {
+  title: 'Teacher Assistant',
+  description: 'Teacher Assistant',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  )
+}
